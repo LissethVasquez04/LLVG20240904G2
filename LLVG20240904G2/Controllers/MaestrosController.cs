02f2629025e9dcbce2f11a5077ec1db43464dad8
LissethVasquez04 <vasquezlisseth004@gmail.com>
2024-09-04 17:13:15 -0600

    controlador creado correctamente

---
+++ LLVG20240904G2/Controllers/MaestrosController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LLVG20240904G2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MaestrosController : ControllerBase
+    {
+        // GET: api/<MaestrosController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<MaestrosController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<MaestrosController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MaestrosController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MaestrosController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
