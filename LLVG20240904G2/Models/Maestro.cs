e108e47e74a3fb8e565721691d1d222e5ed651c0
LissethVasquez04 <vasquezlisseth004@gmail.com>
2024-09-04 17:06:50 -0600

    Se creo la clase Maestro

---
+++ LLVG20240904G2/Models/Maestro.cs
@@ -0,0 +1,20 @@
+﻿namespace LLVG20240904G2.Models
+{
+    public class Maestro
+    {
+        // Identificador único del maestro
+    public int Id { get; set; }
+
+        // Nombre del maestro
+        public string Nombre { get; set; }
+
+        // Apellido del maestro
+        public string Apellido { get; set; }
+
+        // Especialidad o área de enseñanza del maestro
+        public string Especialidad { get; set; }
+
+        // Número de empleado del maestro
+        public string NumeroEmpleado { get; set; }
+    }
+}
