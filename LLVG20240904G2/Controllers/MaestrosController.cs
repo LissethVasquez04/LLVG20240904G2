51149bbfa3154e97b05fb5f5e3589f71f9fcbfe8
LissethVasquez04 <vasquezlisseth004@gmail.com>
2024-09-09 09:24:23 -0600

    Maestro listo

--- LLVG20240904G2/Controllers/MaestrosController.cs
+++ LLVG20240904G2/Controllers/MaestrosController.cs
@@ -12,12 +12,8 @@ namespace LLVG20240904G2.Controllers
 
         static List<Maestro> maestros = new List<Maestro>();
         // GET: api/<MaestrosController>
-        [HttpGet]
-        public IEnumerable<Maestro> Get()
-        {
-            // Devuelve la lista completa de maestros
-            return maestros;
-        }
+    
+         
 
         // GET api/<MaestrosController>/5
         [HttpGet("{id}")]
@@ -44,48 +40,6 @@ namespace LLVG20240904G2.Controllers
             // Devuelve una respuesta OK
             return Ok();
 
-        }
-
-        // PUT api/<MaestrosController>/5
-        [HttpPut("{id}")]
-        public IActionResult Put(int id, [FromBody] Maestro maestro)
-        {// Busca el maestro existente por su ID
-            var existingMaestro = maestros.FirstOrDefault(m => m.Id == id);
-            if (existingMaestro != null)
-            {
-                // Actualiza los datos del maestro existente
-                existingMaestro.Nombre = maestro.Nombre;
-                existingMaestro.Apellido = maestro.Apellido;
-                existingMaestro.Especialidad = maestro.Especialidad;
-                existingMaestro.NumeroEmpleado = maestro.NumeroEmpleado;
-                // Devuelve una respuesta OK
-                return Ok();
-            }
-            else
-            {
-                // Devuelve NotFound si el maestro no existe
-                return NotFound();
-            }
-        }
-
-        // DELETE api/<MaestrosController>/5
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
-        {
-            // Busca el maestro existente por su ID
-            var existingMaestro = maestros.FirstOrDefault(m => m.Id == id);
-            if (existingMaestro != null)
-            {
-                // Elimina el maestro de la lista
-                maestros.Remove(existingMaestro);
-                // Devuelve una respuesta OK
-                return Ok();
-            }
-            else
-            {
-                // Devuelve NotFound si el maestro no existe
-                return NotFound();
-            }
-        }
+        } 
     }
 }
